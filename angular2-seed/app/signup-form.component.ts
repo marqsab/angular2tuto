28eb927846705280280d739653c2bbfa795f793a
Marc <marq22@gmail.com>
2016-05-10 21:47:44 +0200

    validators

--- angular2-seed/app/signup-form.component.ts
+++ angular2-seed/app/signup-form.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {ControlGroup,Control,Validators,FormBuilder} from 'angular2/common'
+import {UsernameValidators} from './usernameValidators';
 
 @Component({
     selector: 'signup-form',
@@ -12,7 +13,10 @@ form:ControlGroup;
 
 constructor(fb : FormBuilder){
   this.form=fb.group({
-    username:['',Validators.required],
+    username:['',Validators.compose([
+                  Validators.required,
+                  UsernameValidators.cannotContainSpace
+                ])],
     password:['',Validators.required]
   })
 }
